c53bb2455735119f8a8ece7df0e2587ae6e9bad0
Mahmoud Galal <mahmoudgalal69@gmail.com>
2024-09-23 20:43:43 +0300

    dsda

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,7 +139,7 @@ export default function Navbar() {
             id="navbar-sticky"
           >
             <ul className="flex  flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
-              {["", "education", "Solutions", "sectors", "our-services"].map(
+              {["", "#education", "#Solutions", "#sectors", "our-services"].map(
                 (menu) => (
                   <li
                     key={menu}
@@ -156,10 +156,10 @@ export default function Navbar() {
                       className="flex items-center  justify-center py-2 px-3 text-gray-500 font-bold rounded hover:bg-gray-100 md:hover:bg-transparent "
                     >
                       {menu === "" && "الرئيسية"}
-                      {menu === "education" && "التعليم"}
-                      {menu === "Solutions" && "الحلول"}
-                      {menu === "sectors" && "القطاعات"}
-                      {menu === "prices" && "الأسعار"}
+                      {menu === "#education" && "التعليم"}
+                      {menu === "#Solutions" && "الحلول"}
+                      {menu === "#sectors" && "القطاعات"}
+                      {menu === "#prices" && "الأسعار"}
                       {menu === "our-services" && "خدماتنا"}
                     </Link>
                     {activeMenu === menu &&
